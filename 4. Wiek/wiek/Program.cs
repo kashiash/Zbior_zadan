d679f601971543c8abdd49ac406e59e970598bc2
Idziku <iddziku@gmail.com>
2018-07-08 15:32:41 +0200

    L4

--- 4. Wiek/wiek/Program.cs
+++ 4. Wiek/wiek/Program.cs
@@ -12,10 +12,23 @@ using System.Threading.Tasks;
     nie to na ekranie pokaze sie "A niech to...".
 
     Dodatkowe zadanie: Stwórz konstruktor domyślny. Zadeklaruj go jawnie.
-
-    podpowiedz:
-    Aby pobrac dzisiejsza date skorzystaj z funkcji DateTime.Now, ktora zwraca obecny rok
+                                                    (jawnie oznacza, że ma być publiczny)
     
+    uwaga,podpowiedz:
+    Aby pobrac dzisiejsza date skorzystaj z funkcji DateTime.Now
+
+    przykladowo:
+    rok mozemy zapisac tak: 
+    double rok = DateTime.Now.Year;
+    
+    uwaga:
+    DateTime.Now jest typu DateTime wiec nie mozemy go wrzucic do odejmowania od tak..
+    i w dodatku DateTime.Now przechowuje nie tylko rok, ale i miesiac, dzien, godziny, minuty 
+    itd... Zatem musimy wyciagnac tylko rok tak jak to pokazano w przykladzie i zapisalismy
+    wynik tego polecenia do zmiennej typu double bo chcemy potem odjąć od niej rok urodzenia
+    osoby X, wiec potrzebujemy roku jako liczby a nie jako lancucha bo moglibysmy rownie 
+    dobrze pobrac rok z DateTime.Now jako tekst. Rozumiesz? :) 
+
     Do tego zadania utrwalisz sobie:
     - jak przypisac wynik liczenia do zmiennej odrazu!
     - jak stworzyć metodę
@@ -24,12 +37,23 @@ using System.Threading.Tasks;
     - jak korzystać z instrukcji warunkowych? Jak są zbudowane?
     - jak stworzyć klasę? 
     */
+
 namespace wiek
 {
+    // tu deklaruj klase
+
     class Program
     {
         static void Main(string[] args)
         {
+            // tu stworz obiekt klasy
+
+            // przykładowe obliczenie wieku dla osoby urodzoenj w 1990
+            double rok = DateTime.Now.Year;
+            double wiek = rok - 1990;
+            Console.Write(wiek);
+
+            Console.ReadLine();
         }
     }
 }
