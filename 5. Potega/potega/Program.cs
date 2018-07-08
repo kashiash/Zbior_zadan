245f32bc5514c6f5457627820b2fb403182c1046
Idziku <iddziku@gmail.com>
2018-07-08 15:46:36 +0200

    L5

--- 5. Potega/potega/Program.cs
+++ 5. Potega/potega/Program.cs
@@ -27,10 +27,12 @@ using System.Threading.Tasks;
     - jak pobrać od użytkownika zmienne? 
     - jak wywołać metodę (funkcję)?
     - jak można zwrócić wynik metody?
-    - jak wypisać na ekran tekst?
+    - jak wypisać na ekran tekst?      
     - pomyśl jak będzie wyglądać kod. Co gdzie umieścić? Zastanów się.
 
-    dla przypomnienia wrzućmy tu konstrukcje pętli:
+    =============================================================
+         dla przypomnienia wrzućmy tu konstrukcje pętli:
+    =============================================================
 
     for (coś ; warunek ; inkrementacja/dekrementacja)   
       {
@@ -41,7 +43,12 @@ using System.Threading.Tasks;
     przykładowo:
     for ( ; i < 100; )...  <= jest prawidłowo
     for (i < 100; )... <= jest nieprawidłowo
-    średniki w pętli for to ŚWIĘTOŚC - sakrament!!!!
+    średniki w pętli for to ŚWIĘTOŚC - sakrament!!!! :D
+
+        for (coś ; warunek ; inkrementacja/dekrementacja)   
+      {
+            instrukcje;
+      }
 
     no i dla przypomnienia omówmy każdy fragment pętli:
     coś - w to miejsce wstawiamy jakieś zmienne jeżeli chcemy które będą "żyły" tylko na potrzeby tej pętli,
@@ -50,8 +57,9 @@ using System.Threading.Tasks;
     inkrementacja/dekrementacja - określasz co ma się zadziać po wykonaniu obrotu w pętli (czy np. zwiększyć o 1 coś
     a może zmniejszyć, a może pomnożyć itd...)
 
-
-    a teraz lecimy z pętla while
+    =============================================================
+                    a teraz lecimy z pętla while
+    =============================================================
     ale zanim Ci pokażę jej szablon to zapamiętaj taki jeden fakcik:
 
     Jaka jest różnica między pętlą while a do...while? - to jest strasznie typowe pytanie i dosyć proste
@@ -68,14 +76,82 @@ using System.Threading.Tasks;
     i finito, tak wygląda pętla while... czyli najpierw sprawdzany jest warunek - jeżeli prawidłowy to lecimy do 
     środka pętli i są wykonywane instrukcje - a jeżeli fałsz w warunku - koniec działania pętli while
 
-    Skoro mowa o pętli while... Możemy ją zrobić nieskończoną - jak? możemy np. napisać
+    Skoro mowa o pętli while... Wspomnijmy o tym, że 
+    możemy ją zrobić nieskończoną - jak? możemy np. napisać
     while(true)
     {
 
     }
-    i nigdy się nie skończy :O , magia, ps; poniżej podałem przykład, sprawdź jeśli chcesz :)
+    i nigdy się nie skończy :O ,
+    magia, ps; poniżej podałem przykład, sprawdź jeśli chcesz :)
     
     i ruszaj potem do zadania =]
+
+    =============================================================
+                       Instrukcje warunkowe 
+    =============================================================
+    przypomnijmy sobie ogólną budowę:
+
+    if(warunek)
+    {
+        instrukcja;
+        instrukcja;
+    }
+    else if(warunek)
+    {
+        instrukcja;
+        instrukcja;
+    }
+    else
+    {
+
+    }
+    To jest ogólna budowa złożona ze wszystkich bloków jakie jesteśmy zapisać
+
+    warto pamiętać, że tylko jeden "blok" instrukcji warunkowej się wykona. 
+    W momencie gdy if, i pozostałe else if nie będą prawdziwe wykona się 
+    fragment "else" 
+
+    #! Fragment else wykona się zawsze gdy wszystko poprzednie zawiedzie!!!
+
+    Pamiętaj, że...
+    Twoje instrukcje warunkowe nie muszą składać się ze wszystkich rzeczy, mogą być
+    bardzo rozbudowane, a mogą być tylko zbudowane z ifa
+
+    <> przykład1:
+    if(x < 100)
+    {
+        Console.WriteLine("X jest mniejsze od 100");
+    }
+
+    <> przyklad2:
+    if(x < 100)
+    {
+        Console.WriteLine("X jest mniejsze od 100");
+    }
+    else if(x > 100)
+    {
+        Console.WriteLine("X jest wieksze od 100");
+    }
+
+
+    <> przyklad3:
+    if(x < 100)
+    {
+        Console.WriteLine("X jest mniejsze od 100");
+    }
+    else if(x > 100)
+    {
+        Console.WriteLine("X jest wieksze od 100");
+    }
+    else
+    {
+        Console.WriteLine("halo??");
+    }
+
+    Instrukcje warunkowe są szczególnie przydatne w pętlach gdy np. w określonym momencie
+    coś chcemy zrobić... np. wyjść z pętli :) 
+
     */
 namespace potega
 {
@@ -87,7 +163,8 @@ namespace potega
 
             /* Przykładowy nieskończony program:
              * Sprawdź go, jeżeli chcesz, tylko usuń
-             * komentarze albo przekopiuj go niżej 
+             * komentarze albo przekopiuj sam kod niżej 
+             
             int zlicz = 1;
             while(true)
             {
@@ -95,7 +172,11 @@ namespace potega
                 zlicz++;
                 Thread.Sleep(500);
             }
+
             */
+
+
+            Console.ReadLine();
         }
     }
 }
