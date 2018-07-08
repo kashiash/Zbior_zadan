b6cf920d92acfea4b7a0d5a73d53170b5f3df9d1
Idziku <iddziku@gmail.com>
2018-07-08 16:06:21 +0200

    L

--- 10. Potega/potega_rek_ite/Program.cs
+++ 10. Potega/potega_rek_ite/Program.cs
@@ -28,6 +28,35 @@ using System.Threading.Tasks;
     - Jakie wyróżniamy pętle? 
     - Co to jest rekurencja?
     - Czy rekurencja musi mieć jakiś warunek?
+
+    #! Podpowiedzi:
+
+    Potega iteracyjnie to musimy stworzyć pętlę np. for
+    jak użytkownik poda że chce 2 do 3 podnieść to ta petla musi sie
+    wykonac 3 razy i bedzie robic tak: 
+    pierwszy obrót: 2*2 = 4
+    drugi obrót: 4*2 = 8
+    trzeci obrót: 8*2 = 16 
+    i mamy :)
+
+    Potega rekurencyjnie
+    (rekurencyjnie czyli funkcja musi wywolywac sama siebie) 
+    - musisz stworzyc funkcje ktora bedzie zwracac siebie sama
+    np. return x * rekurencja(x, n-1)
+    x to podstawa
+    n to wykladnik
+
+    przykładowe działanie rekurencyjne:
+    podajemy że chcemy podniesc 2(x) do potegi 3(n) to wtedy twoja funkcja powinna zadzialac tak:
+    2 * rekurencja(2,3-1)
+    2 * 2 * rekurencja(2,2-1)
+    2 * 2 * 2 * rekurencja(2,1-1)
+    i musimy dodac warunek w funkcji na poczatku że if (n == 0 ) zwroc np. 1 bo jak pomnozymy przez 1 
+    to nie zmieni to wyniku prawda? Wiec ostatecznie bedzie tak:
+    2 * 2 * 2 * 1 = 8 - dziala :0 :)
+
+    w razie gdyby byly nadal z tym problemy zerknij na rozwiazanie tutaj:
+    https://github.com/trolit/Programowanie/blob/master/potega_rekurencyjnie/potega_rekurencyjnie.cpp
 */
 namespace potega_rek_ite
 {
@@ -35,6 +64,8 @@ namespace potega_rek_ite
     {
         static void Main(string[] args)
         {
+
+            Console.ReadLine();
         }
     }
 }
